7228dc5f2e762052a18afbc57c85b74abe46b1ea
Herman Streltsov <mail@streltsov.dev>
2023-10-08 14:16:00 +0200

    Remove `/blog` prefix from URL's

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -1,6 +1,6 @@
 module.exports = {
-	tags: [
-		"posts"
-	],
-	"layout": "layouts/post.njk",
+	tags: ["posts"],
+	layout: "layouts/post.njk",
+	// Making the URL (`domain.com/some-post`), not (`domain.com/blog/some-post`).
+	permalink: "/{{ page.fileSlug }}/",
 };
